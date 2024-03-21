36dc24493cf1f154cc3d1cbb28757dc0389ef4f1
Steven <stevenhuebner94@gmail.com>
2024-03-21 20:13:05 +0100

    Initial Commit

---
+++ GronkTVApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GronkTVApp
+//
+//  Created by Steven Hübner on 21.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GronkTVApp/GronkTVAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GronkTVAppApp.swift
+//  GronkTVApp
+//
+//  Created by Steven Hübner on 21.03.24.
+//
+
+import SwiftUI
+
+@main
+struct GronkTVAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
